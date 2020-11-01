44e9f2f1572f1fd36b30623a1f5318c52a383a9c
Aleksei <shinizlc@gmail.com>
2020-11-01 18:32:58 +0300

    1:5 modification 2

--- LESSON2/task2.py
+++ LESSON2/task2.py
@@ -1,11 +1,10 @@
 l = []
+while True:
+    elem = input('Enter your value: ')
+    if elem =='': ###почему не работает is None
+        break
+    l.append(elem)
 
-# elem = input('Enter your value: ')
-#
-# l.append(elem)
-
-l=[1,2,3,4,5,6]
 for i in range(0,len(l)-1,2):
-    print(i)
     l[i],l[i+1]=l[i+1],l[i]
 print(l)
--- LESSON2/task5.py
+++ LESSON2/task5.py
@@ -1,30 +1,17 @@
 
 my_list = [7, 5, 3, 3, 2]
-
 print(my_list)
 while True:
-    # try:
-    user_input = abs(int(input('Enter your value: ')))
-    for k, i in enumerate(my_list):
-        # print(k)
-        # print(i)
-        if user_input ==i:
-            my_list.insert(k+1, user_input)
-            print('po uslovi <')
-            break
-        elif user_input<i:
-            print(k)
-            print(user_input)
-            my_list.insert(k + 1, user_input)
-            print('po uslovi >')
-            #my_list.append(user_input)
+    try:
+        user_input = abs(int(input('Enter your value: ')))
+        for l in range(0,len(my_list)-1):
+            v,v_1=my_list[l],my_list[l+1]
+            if user_input <=v and user_input>=v_1:
+                my_list.insert(l+1, user_input)
+                break
+            if user_input<min(my_list):
+                my_list.append(user_input)
 
-        else:
-            pass
-        # else:
-        #     my_list.insert(0,user_input)
-        #     print('po uslovi 0')
-    print(my_list)
-    # except ValueError:
-    #     print('Enter the numeric value!')
-    #     exit(255)
+        print(my_list)
+    except ValueError:
+         print('Enter the numeric value!')
