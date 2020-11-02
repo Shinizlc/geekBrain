ab5ba5ce8bf701eddbb919ca0a8261e5d3423290
Aleksei <shinizlc@gmail.com>
2020-11-02 11:47:00 +0300

    task 3:6

--- LESSON2/task6.py
+++ LESSON2/task6.py
@@ -1,2 +1,26 @@
-l=[]
-k=0
+# l=[]
+# c=0
+# template={'Name':'Enter a name of an item: \n',
+#           'Price':'Enter the price: \n',
+#           'Quanitity':'Enter the quantity:\n '}
+#
+#
+# while True:
+#     d = {}
+#     for k, v in template.items():
+#         d[k] = input(v)
+#         # if d[k]=='':
+#         #     break
+#     l.append((c,d))
+#     c+=1
+#     print(l)
+l=[(0, {'Name': 'comp', 'Price': '43', 'Quanitity': '3'}),
+ (1, {'Name': 'calc', 'Price': '32', 'Quanitity': '2'})]
+l_1=[]
+dic_analyze={}
+for i in l:
+    dic=i[1]
+    for j,m in dic.items():
+        #dic_analyze[j] = (m)
+        dic_analyze[j] = dic_analyze.get(j, []) + [m]
+print(dic_analyze)
