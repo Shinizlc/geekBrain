62a036e0dee410b57c3e9175493b461947ab8c35
Aleksei <shinizlc@gmail.com>
2020-11-05 11:14:34 +0300

    1,3

--- LESSON3/task1.py
+++ LESSON3/task1.py
@@ -1,31 +1,3 @@
-# next=True
-# while True:
-#     answer = int(input('insert 1 or 0\n'))
-#     if not answer:
-#         next=False
-#         break
-#     else:
-#         next=True
-#
-# # print(bool(int(0)))
-
-#
-# def print_dic(name,last_name):
-#     return f'fullname: {name} {last_name}'
-# dic=[{'name':'Aleksei','last_name':'Semerikov'},{'name':'Nikoloi','last_name':'Sovalev'}]
-# l=[print_dic(**i) for i in dic if i['name']=='Aleksei']
-# print(l)
-# c = [1,2,3,4,5]
-# def print_global():
-#     #global c
-#     c=[]
-#     c.append('5454')
-#     return c
-#
-# result=print_global()
-# print(result)
-# print(c)
-
 def division (a:int,b:int)-> float:
     """function for division 2 numbers"""
     try:
@@ -44,5 +16,5 @@ def division (a:int,b:int)-> float:
 
 # print(division.__doc__)
 
-assert division(1,1)==1
-assert division(2,0)==None
+# assert division(1,1)==1
+# assert division(2,0)==None
